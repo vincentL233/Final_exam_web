1e895ca04050e0cb39e0d9c6519e21a39c22a2db
vincentL233 <vincent02282010@gmail.com>
2026-01-07 00:09:32 +0800

    1/6v3

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -18,5 +18,18 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 1500, // 將警告限制調大到 1500kb (因為用到 Three.js)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // 將 node_modules 中的程式碼單獨打包 (例如 three.js, gsap)
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
